36388e805a7d21003878605bf8dc3d9ef0b66372
Esknor <volodimirvrabets@gmail.com>
2018-07-11 14:04:27 +0300

    ex2

--- ex2/cipher.go
+++ ex2/cipher.go
@@ -4,3 +4,165 @@ type Cipher interface {
 	Encode(string) string
 	Decode(string) string
 }
+
+type MyCaesar struct{}
+
+type MyShift struct {
+	shift int
+}
+
+type MyVigenere struct {
+	key string
+}
+
+func NewCaesar() Cipher {
+	return MyCaesar{}
+}
+
+func NewShift(shift int) Cipher {
+	if shift > 25 || shift < -25 || shift == 0 {
+		return MyShift{0}
+	}
+	return MyShift{shift}
+}
+
+func (c MyCaesar) Encode(s string) string {
+	str := make([]byte, len(s))
+	for i := 0; i < len(s); i++ {
+		if s[i] > 64 && s[i] < 91 {
+			str[i] = s[i] + 32 + 3
+		} else if s[i] > 96 && s[i] < 123 {
+			str[i] = s[i] + 3
+		} else {
+			continue
+		}
+	}
+	return string(str)
+}
+
+func (c MyCaesar) Decode(s string) string {
+	str := make([]byte, len(s))
+	for i := 0; i < len(s); i++ {
+		if s[i] != 0 {
+			str[i] = s[i] - 3
+		}
+	}
+	return string(str)
+}
+
+func (shift MyShift) Encode(s string) string {
+	str := make([]byte, len(s))
+	for i := 0; i < len(s); i++ {
+		if s[i] > 64 && s[i] < 91 {
+			str[i] = s[i] + 32 + byte(shift.shift)
+		} else if s[i] > 96 && s[i] < 123 {
+			str[i] = s[i] + byte(shift.shift)
+		} else {
+			continue
+		}
+	}
+	return string(str)
+}
+
+func (shift MyShift) Decode(s string) string {
+	str := make([]byte, len(s))
+	for i := 0; i < len(s); i++ {
+		if s[i] != 0 {
+			str[i] = s[i] - byte(shift.shift)
+		}
+	}
+	return string(str)
+}
+
+func (v MyVigenere) Encode(p string) string {
+	s := ReadyToEncode(p)
+	str := make([]byte, len(s))
+
+	for i := 0; i < len(s); i++ {
+		tmp := s[i] + uint8(v.key[i%len(v.key)]) - uint8('a')
+
+		if tmp > 122 {
+			tmp -= 26
+		}
+		str[i] = tmp
+	}
+
+	return string(str)
+}
+
+func (v MyVigenere) Decode(s string) string {
+	str := make([]byte, len(s))
+
+	for i := 0; i < len(s); i++ {
+		tmp := s[i] - uint8(v.key[i%len(v.key)]) + uint8('a')
+
+		if tmp < 97 {
+			tmp += 26
+		}
+
+		str[i] = tmp
+	}
+
+	return string(str)
+}
+
+func NewVigenere(key string) Cipher {
+	if !invalid_key(key) {
+		return nil
+	}
+
+	return MyVigenere{key}
+}
+
+func ReadyToEncode(s string) string {
+	count := 0
+	j := 0
+
+	for i := 0; i < len(s); i++ {
+		if (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') {
+			count++
+		}
+	}
+
+	str := make([]byte, count)
+
+	for i := 0; i < len(s); i++ {
+		if (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') {
+			if s[i] >= 'A' && s[i] <= 'Z' {
+				str[j] = s[i] - 'A' + 'a'
+			} else {
+				str[j] = s[i]
+			}
+			j++
+		}
+	}
+	return string(str)
+}
+
+func invalid_key(s string) bool {
+	flag := false
+
+	if len(s) < 1 {
+		return false
+	}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == ' ' || (s[i] > 40 && s[i] < 91) {
+			return false
+		}
+
+		if s[i] != 'a' {
+			flag = true
+		}
+	}
+	return flag
+}
+
+/*func main() {
+	s := "Go, go, gophers"
+	var c Cipher = NewCaesar()
+	shift := NewShift(3)
+	fmt.Println(MyShift{shift})
+	fmt.Println(c.Encode(s))
+	fmt.Println(c.Decode(c.Encode(s)))
+}*/

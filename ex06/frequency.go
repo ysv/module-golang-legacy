8b562fba0771f020e628c2dae4435ba8b58d2d55
Denis Fedorchenko <dfedorchenko@heliostech.fr>
2018-07-11 13:34:53 +0300

    Ex06 solved

---
+++ ex06/frequency.go
@@ -0,0 +1,43 @@
+package letter
+
+import "sync"
+
+func Frequency(text string) map[rune]int {
+	m := map[rune]int{}
+
+	for _, v := range text {
+		m[v] += 1
+	}
+
+	return m
+}
+
+type SafeMap struct {
+	fmap  map[rune]int
+	mutex *sync.Mutex
+}
+
+func concFrequency(text string, fmap *SafeMap, done chan byte) {
+	for _, v := range text {
+		fmap.mutex.Lock()
+		fmap.fmap[v] += 1
+		fmap.mutex.Unlock()
+	}
+	done <- 1
+}
+
+func ConcurrentFrequency(texts []string) map[rune]int {
+	fmap := &SafeMap{fmap: map[rune]int{}, mutex: &sync.Mutex{}}
+	len := len(texts)
+	done := make(chan byte, len)
+
+	for _, text := range texts {
+		go concFrequency(text, fmap, done)
+	}
+
+	for i := 0; i < len; i++ {
+		<-done
+	}
+
+	return fmap.fmap
+}

1c2d77035fee2af7cacc88b6d67c7554f6b42f8c
Esknor <volodimirvrabets@gmail.com>
2018-07-18 13:29:24 +0300

    07

--- ex07/goroutines.go
+++ ex07/goroutines.go
@@ -1,46 +1,19 @@
-//package goroutines
-package main
-
-import (
-	"fmt"
-	//"time"
-	"sync"
-)
-
-var wg sync.WaitGroup
+package goroutines
 
 func Process(input chan string) chan string {
-	s := <-input
 	var output chan string = make(chan string)
-	wg.Add(1)
-	go in(s, output)
-	wg.Wait()
-	go out(input)
+	var done chan bool = make(chan bool)
+
+	go func() {
+		str := "(" + <-input + ")"
+		output <- string(str)
+		done <- true
+	}()
+
+	go func() {
+		<-done
+		close(output)
+	}()
 
 	return output
-}
-
-func in(s string, output chan string) {
-	defer wg.Done()
-	str := make([]byte, len(s)+2)
-	str[0] = 40
-	for i := 0; i < len(s); i++ {
-		fmt.Println(string(str))
-		str[i+1] = s[i]
-		if i+2 > len(s) {
-			str[i+2] = 41
-		}
-	}
-	fmt.Println(string(str))
-	output <- string(str)
-}
-
-func out(input chan string) {
-	close(input)
-}
-
-func main() {
-	var input chan string = make(chan string)
-	input <- "hello world!"
-	Process(input)
 }

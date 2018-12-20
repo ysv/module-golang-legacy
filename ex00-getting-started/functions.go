6a37df10e12c7fc9fcde122221c3aafebceaded6
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2018-12-21 00:00:55 +0200

    Fix codestyle using gofmt

--- ex00-getting-started/functions.go
+++ ex00-getting-started/functions.go
@@ -5,12 +5,12 @@ import "fmt"
 // Go's return values may be named.
 // If so, they are treated as variables defined at the top of the function.
 // 'Naked' return.
-func split(sum int)(x, y int){
+func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
 	return
 }
 
-func main(){
+func main() {
 	fmt.Println(split(17))
 }
--- ex00-getting-started/vars.go
+++ ex00-getting-started/vars.go
@@ -6,24 +6,24 @@ import (
 	"math/cmplx"
 )
 
-var(
-	ToBe bool = false
-	MaxInt int = 1<<63 - 1
-	z complex128 = cmplx.Sqrt(-5 + 12i)
+var (
+	ToBe   bool       = false
+	MaxInt int        = 1<<63 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
-func typeCasting(){
+func typeCasting() {
 	a, b := 3, 4
 	c := math.Sqrt(float64(a*a + b*b))
 	fmt.Printf("Type: %T. Value %v \n", c, c)
 }
 
-func constants(){
+func constants() {
 	const Pi = 3.14
 	fmt.Println("Happy", Pi, "Day")
 }
 
-func main(){
+func main() {
 	fmt.Printf("Type: %T. Value %v \n", ToBe, ToBe)
 	fmt.Printf("Type: %T. Value %v \n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T. Value %v \n", z, z)
